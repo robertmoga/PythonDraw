feb6c92ff9d7c7ed1bbb2a13f1f48c84884115e4
Robert Moga <robertmoga16@gmail.com>
2018-05-25 18:34:13 +0300

    Finished word segmentation, still need to do line and word merge in case of close elements,

--- PyDraw/dev/data_processing.py
+++ PyDraw/dev/data_processing.py
@@ -52,34 +52,29 @@ class ImageAnalyser:
         erosion = cv2.erode(closing, kernel2, iterations=2)
 
         img = cv2.cvtColor(erosion, cv2.COLOR_RGB2GRAY)
-
         return img
 
-        # define words : UNTESTED
+
+
+    #define words
     def execute_morpho_on_ox(self):
-        im2, contours, hierarchy = cv2.findContours(self.raw_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        img = cv2.cvtColor(self.raw_image, cv2.COLOR_GRAY2RGB)
+        img = self.raw_image
+        img = cv2.convertScaleAbs(img, alpha=(255.0 / 65535.0))
+        im2, contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
         cv2.fillPoly(img, contours, color=(255, 255, 255))
 
         # kernels for morphological operations
-        kernel1 = np.ones((2, 30))
+        kernel1 = np.ones((30, 2))
         kernel2 = np.ones((3, 3))
 
         dilation = cv2.dilate(img, kernel2, iterations=1)
         closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel1)
         erosion = cv2.erode(closing, kernel2, iterations=2)
 
-        return erosion
+        img = cv2.cvtColor(erosion, cv2.COLOR_RGB2GRAY)
+        return img
 
-    # @property
-    # def bounds(self):
-    #     self._bounds = self.analyse_image()
-    #     return self._bounds
-    #
-    # @bounds.setter
-    # def bounds(self, value):
-    #     if isinstance(value, list):
-    #         self._bounds = value
 
     def compute_projection_oy(self, morpho_image):
 
@@ -89,6 +84,25 @@ class ImageAnalyser:
             for j in range(self.raw_image.shape[1]):
                 if morpho_image[i][j] > 0:
                     oy_values[i] += 1
+        return oy_values
+
+    def compute_projection_ox(self, morpho_image):
+        oy_values = list()
+        # print(morpho_image.shape)
+        # print(morpho_image[328][47])
+        for i in range(self.raw_image.shape[1]):
+            oy_values.append(0)
+            for j in range(self.raw_image.shape[0]):
+                # print(str(i) + " , " + str(j))
+                # if i == 88:
+                #     print('hue')
+                try:
+                    if morpho_image[j][i] > 0:
+                        pass
+                        oy_values[i] += 1
+                except Exception as e:
+                    print(">> " + str(i) +" , " +str(j))
+                    print(str(e))
         return oy_values
 
     def get_bounds(self, values):
@@ -161,6 +175,46 @@ class LineBuilder(ImageAnalyser):
         return new_elem
 
 
+class WordBuilder(ImageAnalyser):
+    def __init__(self, raw_img):
+        ImageAnalyser.__init__(self, raw_img=raw_img)
+        self.raw_image = raw_img
+        self.bounds = self.__build_bounds()
+        self.elements = self.build_elements()
+
+    def __build_bounds(self):
+        img_morpho = self.execute_morpho_on_ox()
+        ox_values = self.compute_projection_ox(img_morpho)
+        bounds = self.get_bounds(ox_values)
+
+        return bounds
+
+    def build_elements(self, verbose=0):
+        elements = list()
+        width_negative_space = 10
+
+        if verbose == 1:
+            print(">> Width of added negative space : " + str(width_negative_space))
+
+        for elem in self.bounds:
+
+            new_elem = self.raw_image[:, elem[0]:elem[1]]
+            if verbose == 1:
+                print(">> Current raw elem shape " + str(new_elem.shape))
+            new_elem_np = self.__add_negative_space(new_elem, width_negative_space)
+            new_elem = Element(img=new_elem_np, type='word')
+            elements.append(new_elem)
+
+        return elements
+
+    def __add_negative_space(self, element, width):
+
+        space = np.zeros((self.raw_image.shape[0], width))
+        new_elem = np.hstack((space, element))
+        new_elem = np.hstack((new_elem, space))
+        return new_elem
+
+
 
 def test(img):
     im2, contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -177,6 +231,7 @@ def test(img):
     ImageNormaliser.plotData(img)
     return img
 
+
 if __name__ == "__main__":
 
     data_reader = DataToImage("tempFiles/fis.txt", "tempFiles/newImage.png")
@@ -187,12 +242,12 @@ if __name__ == "__main__":
     line_builder = LineBuilder(image)
     lines = line_builder.elements #array of line Elements
 
-    for i in lines:
-        ImageNormaliser.plotData(i.image)
+    words = list()
+    for line in lines:
+        word_builder = WordBuilder(line.image)
+        words.append(word_builder.elements)
 
-    # print(line_builder.bounds)
+    for line in words:
+        for word in line:
+            ImageNormaliser.plotData(word.image)
 
-    # ImageNormaliser.plotData(img)
-
-    # img = test(image)
-    # ImageNormaliser.plotData(img)
